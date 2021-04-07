a967ca137afa6f9fbc6ae038e14c60885193d7ef
mikey <michael.pandu@ti.ukdw.ac.id>
2021-04-07 08:40:02 +0700

    cars commit

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -15,6 +15,35 @@ public class CarTest {
     @Test
     public void testDefaultValueOfMyCar(){
         assertFalse("my car needs fuel", myCar.needsFuel());
-        assertEquals("my car engine temperature should be 0.0 as double", 0.0, myCar.getEngineTemperature(), 1e-3);
+        when(myCar.needsFuel()).thenReturn(true);
+        assertTrue("my car needs fuel", myCar.needsFuel());
+//        assertEquals("my car engine temperature should be 0.0 as double", 0.0, myCar.getEngineTemperature(), 1e-3);
+    }
+
+    @Test
+    public void testMyCarHaveException() {
+        assertFalse("my car needs fuel", myCar.needsFuel());
+        when(myCar.needsFuel()).thenThrow(new RuntimeException());
+        assertTrue("my car needs fuel", myCar.needsFuel());
+    }
+
+    @Test
+    public void testVerification(){
+        myCar.driveTo("Sweet Campus UKDW");
+        myCar.needsFuel();
+        verify(myCar).driveTo(any());
+        verify(myCar).needsFuel();
+    }
+
+    @Test
+    public void testVerificationFailure(){
+        myCar.needsFuel();
+        verify(myCar).getEngineTemperature();
+    }
+
+    @Test
+    public void testVerificationFailureArguments(){
+        myCar.driveTo("Sweet Campus UKDW");
+        verify(myCar).driveTo("Home Sweet Home");
     }
 }
